bcfbf3c64d1a39595b31226c4a706e9893d8d151
HichTala <98521878+HichTala@users.noreply.github.com>
2023-12-16 17:59:29 +0100

    Bill acceptor

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def read(ser):
     out = []
     while ser.inWaiting() > 0:
         out.append(ser.read(1))
+    print("Answer:")
+    print([binascii.hexlify(b).decode() for b in out])
     return out
 
 
@@ -19,6 +21,10 @@ def send(ser, cmd):
     for b in send:
         cs += b
     send[4] = cs % 256
+
+    send_bytes = [b.to_bytes(1, 'big') for b in send]
+    print("Sent bytes:")
+    print([binascii.hexlify(b).decode() for b in send_bytes])
 
     ser.write(send)
     time.sleep(0.1)
@@ -33,19 +39,25 @@ def main(portname):
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE
     )
-    currency_dict = {1: 0.5, 2: 1, 3: 2}
+    # currency_dict = {1: 0.5, 2: 1, 3: 2}
+    currency_dict = {254: 2000, 255: 1000}
     amount = None
 
-    send(ser, 0x02)
-
     print("Order amount to pay?")
+    out = []
+    while not out:
+        ser.write([0x5e])
+        while ser.inWaiting() > 0:
+            out.append(ser.read(1))
 
     while ser.isOpen():
         try:
             if amount is None:
                 amount = float(input())
                 print("Still to be paid:", amount)
-                send(ser, 0x01)
+                ser.write([0x3e])
+
+            ser.write([0x02])
 
             out = []
             while ser.inWaiting() > 0:
@@ -54,14 +66,22 @@ def main(portname):
             if not out:
                 continue
 
-            status = currency_dict[ord(out[3])]
+            if len(out) <= 1 or (not (ord(out[1]) in [0xfe, 0xff])):
+                continue
+
+            status = currency_dict[ord(out[1])]
 
             if amount is not None:
                 amount -= status
                 print("Amount collected", status)
                 if amount <= 0:
+                    out = []
+                    while not out:
+                        ser.write([0x5e])
+                        while ser.inWaiting() > 0:
+                            out.append(ser.read(1))
                     print("Currency to be returned", -amount)
-                    send(ser, 0x02)
+                    break
                 else:
                     print("Still to be paid:", amount)
 
